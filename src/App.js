574ca940be842856a829420bff195c1d3cf1bc91
Fahmi-IT <sajidfahmi03@gmail.com>
2023-03-14 10:45:39 -0400

    basic design, loading screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import PreLoader1 from "./components/PreLoader1"
 
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+        <header className="AppHeader">
+            <PreLoader1/>
+        </header>
     </div>
-  );
+  )
 }
 
 export default App;
