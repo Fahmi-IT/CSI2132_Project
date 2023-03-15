61ab6194c72188759bf82118ba23ee1d7b098edb
Fahmi-IT <98490159+Fahmi-IT@users.noreply.github.com>
2023-03-15 12:44:27 -0400

    triggers v3
    finalized set of triggers with accommodations to changes in db implementation. will bee tested. v5 tbd.

--- triggers.sql
+++ triggers.sql
@@ -1,23 +1,55 @@
-CREATE OR REPLACE FUNCTION update_Hotel_Count()
-	RETURNS TRIGGER AS
-$BODY$
+CREATE OR REPLACE FUNCTION decrease_Hotel_Count() /*Decreases number_of_hotels attribute in hotel_chain*/
+	RETURNS TRIGGER AS							  /*depending on the number of hotels deleted/dropped*/
+$$
+DECLARE
+	deleted_count integer; --declares intermediate variable to be used
 BEGIN
-	UPDATE hotel_chain
-	SET number_of_hotels = number_of_hotels - 1
-	WHERE name = OLD.name;
-	RETURN OLD;
+	SELECT count(*) FROM old INTO deleted_count; --stores number of deleted rows
+	UPDATE hotel_chain SET number_of_hotels = number_of_hotels - deleted_count WHERE old.name = hotel_chain.name;
+	RETURN NULL;
 END;
-$BODY$
+$$
 LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION update_Room_Count()
-	RETURNS TRIGGER AS
-$BODY$
+CREATE OR REPLACE FUNCTION decrease_Room_Count() /*Decreases number_ofrooms attribute in hotel*/
+	RETURNS TRIGGER AS							 /*depending on the number of rooms deleted/dropped*/
+$$
+DECLARE
+	deleted_count integer; --declares intermediate variable to be used
+BEGIN
+	SELECT count(*) FROM old INTO deleted_count; --stores number of deleted  rows
+	UPDATE hotel SET number_of_rooms = number_of_rooms - deleted_count WHERE old.hotel_ID = hotel.hotel_ID;
+	RETURN NULL;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER update_Hotel_Chain --trigger on hotel deletion
+AFTER DELETE ON hotel
+FOR EACH ROW
+EXECUTE PROCEDURE decrease_Hotel_Count(); --executes function
+
+CREATE TRIGGER update_Hotel --trigger on room deletion
+AFTER DELETE ON room
+FOR EACH ROW
+EXECUTE PROCEDURE decrease_Room_Count(); --executes function
+
+CREATE OR REPLACE FUNCTION increase_Hotel_Count()
+RETURNS TRIGGER AS $$
+BEGIN
+	UPDATE hotel_chain
+	SET number_of_hotels = number_of_hotels + New.num_elements_added
+	WHERE hotel_chain.name = NEW.name;
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION increase_Room_Count()
+RETURNS TRIGGER AS $$
 BEGIN
 	UPDATE hotel
-	SET number_of_rooms = number_of_rooms - 1
-	WHERE name = OLD.name;
-	RETURN OLD;
+	SET number_of_rooms = number_of_rooms + New.num_elements_added
+	WHERE hotel.hotel_id = NEW.hotel_id;
+	RETURN NEW;
 END;
-$BODY$
-LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql;
