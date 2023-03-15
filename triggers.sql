c64935816f61075b4ae429425c289f409fb77992
Fahmi-IT <98490159+Fahmi-IT@users.noreply.github.com>
2023-03-14 20:21:49 -0400

    triggers v2
    includes functions used for the triggers, need to test one and implement trigger queries, otherwise done

--- triggers.sql
+++ triggers.sql
@@ -1,9 +1,21 @@
-CREATE OR REPLACE FUNCTION update_table2()
+CREATE OR REPLACE FUNCTION update_Hotel_Count()
 	RETURNS TRIGGER AS
 $BODY$
 BEGIN
 	UPDATE hotel_chain
 	SET number_of_hotels = number_of_hotels - 1
+	WHERE name = OLD.name;
+	RETURN OLD;
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION update_Room_Count()
+	RETURNS TRIGGER AS
+$BODY$
+BEGIN
+	UPDATE hotel
+	SET number_of_rooms = number_of_rooms - 1
 	WHERE name = OLD.name;
 	RETURN OLD;
 END;
