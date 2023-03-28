d278a48d45bfd588046e3a1d78bece99c1e92731
Fahmi-IT <sajidfahmi03@gmail.com>
2023-03-28 16:30:46 -0400

    basic signup/login skeleton

--- src/components/pages/signup.js
+++ src/components/pages/signup.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Login} from './login_register/Login';
+import {Register} from './login_register/Register';
+import './signup.css'
   
-const SignUp = () => {
+function SignUp() {
+  const [currentForm, setCurrentForm] = useState('login');
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'Right',
-        alignItems: 'Right',
-        height: '100vh'
-      }}
-    >
-      <h1>Sign Up</h1>
+    <div className="loginPg">
+      {currentForm === 'login' ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />}
     </div>
   );
 };
