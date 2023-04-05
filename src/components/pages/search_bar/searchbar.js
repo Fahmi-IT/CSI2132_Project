7f4ca86d34f93e5bb8e9fff149318ea327fc1dd2
Fahmi-IT <sajidfahmi03@gmail.com>
2023-04-05 12:28:59 -0400

    finish searchbar (skeleton done)

--- src/components/pages/search_bar/searchbar.js
+++ src/components/pages/search_bar/searchbar.js
@@ -3,33 +3,148 @@ import "./searchbar.css";
 
 function SearchBar() {
   var roomers = [];
+  var Hhotels = [];
+  var Hhotel_chains = [];
+  var Bbookings = [];
+  const [bookings, setBookings] = useState(Bbookings);
+  const [hotelChain, setHotelChain] = useState(Hhotel_chains);
+  const [hotels, setHotels] = useState(Hhotels);
   const [rooms, setRooms] = useState(roomers);
   const [capacity, setCapacity] = useState('');
   const [price, setPrice] = useState('');
+  const [rating, setRating] = useState('');
+  const [numRooms, setNumRooms] = useState('');
+  const [area, setArea] = useState('');
+  const [chain, setChain] = useState('');
+  const [startY, setStartY] = useState('');
+  const [startM, setStartM] = useState('');
+  const [startD, setStartD] = useState('');
+  const [endY, setEndY] = useState('');
+  const [endM, setEndM] = useState('');
+  const [endD, setEndD] = useState('');
   const handleCapacityChange = (e) => {
     setCapacity(e.target.value);
     console.log(capacity);
   };
   const handlePriceChange = (e) => {
     setPrice(e.target.value);
-    console.log(price);
+  }
+  const handleRatingChange = (e) => {
+    setRating(e.target.value);
+  }
+  const handleNumRoomsChange = (e) => {
+    setNumRooms(e.target.value);
+  }
+  const handleAreaChange = (e) => {
+    setArea(e.target.value);
+  }
+  const handleChainChange = (e) => {
+    setChain(e.target.value);
+  }
+  const handleStartYChange = (e) => {
+    setStartY(e.target.value);
+  }
+  const handleStartMChange = (e) => {
+    setStartM(e.target.value);
+  }
+  const handleStartDChange = (e) => {
+    setStartD(e.target.value);
+  }
+  const handleEndYChange = (e) => {
+    setEndY(e.target.value);
+  }
+  const handleEndMChange = (e) => {
+    setEndM(e.target.value);
+  }
+  const handleEndDChange = (e) => {
+    setEndD(e.target.value);
   }
   useEffect(() => {
     getRooms();
+    getHotels();
+    getHotelChains();
+    getBookings();
   }, []);
+  function findHotel(room) {
+    for (let i = 0; i < hotels.length; i++) {
+      if (hotels[i].hotelID === room.hotel_ID) {
+        return hotels[i];
+      }
+    }
+    return false;
+  }
+  function findHotelChain(holte) {
+    for (let i = 0; i < hotelChain.length; i++) {
+      if (holte.name === hotelChain[i].name) {
+        return hotelChain[i];
+      }
+    }
+    return false;
+  }
+  function findBooking(room) {
+    var from = 0;
+    var to = 1;
+    if (startY != null && startM != null && startD != null) {
+      from = new Date(startY, startM, startD);
+    }
+    if (endY != null && endM != null && endD != null) {
+      to = new Date(endY, endM, endD);
+    }
+    if (from === 0) {
+      return false;
+    } else if (from > to) {
+      return false;
+    }
+    for (let i = 0; i < bookings.length; i++) {
+      if (bookings[i].roomNumber === room.roomNumber) {
+        let pre = new Date(bookings[i].startDate.substring(0, 4), bookings[i].startDate.substring(5, 7), bookings[i].startDate.substring(8, 10));
+        let post = new Date(bookings[i].endDate.substring(0, 4), bookings[i].endDate.substring(5, 7), bookings[i].endDate.substring(8, 10));
+        if (from < post || to > pre) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
   function passFilter(room) {
-    var resulting = true;
+    let temp = findHotel(room);
+    let chainz = findHotelChain(temp);
     if (capacity != null) {
       if (parseInt(room.capacity) < parseInt(capacity)) {
-        resulting = false;
+        return false;
       }
     }
     if (price != null) {
       if (parseInt(room.PPN) > parseInt(price)) {
-        resulting = false;
+        return false;
       }
     }
-    return resulting;
+    if (rating != null) {
+      if (parseInt(temp.starRating) < parseInt(rating)) {
+        return false;
+      }
+    }
+    if (numRooms != null) {
+      if (parseInt(temp.numberOfRooms) < parseInt(numRooms)) {
+        return false;
+      }
+    }
+    if (area != null && area.trim() !== "") {
+      if (!(temp.address.toLowerCase().includes(area.trim().toLowerCase()))) {
+        return false;
+      }
+    }
+    if (chain != null && chain.trim() !== "") {
+      if (!(chainz.name.toLowerCase().includes(chain.trimEnd().toLowerCase()))) {
+        return false;
+      }
+    }
+    if (startY != null && startM != null && startD != null && endY != null && endM != null && endD != null) {
+      if (!(findBooking(room))) {
+        return false;
+      }
+    }
+    return true;
   }
   function getRooms() {
     fetch('http://localhost:3001/').then(response => {return response.text();})
@@ -55,6 +170,64 @@ function SearchBar() {
         setRooms(roomers);
       });
   }
+  function getHotels() {
+    fetch('http://localhost:3001/hotel').then(response => {return response.text();})
+    .then(data => {
+      const obj = JSON.parse(data);
+      Hhotels = [];
+      for (let i = 0; i < obj.length; i++) {
+        let temp = obj[i];
+        let hotel = {
+          hotelID: temp["hotel_id"],
+          name: temp["name"],
+          address: temp["address"],
+          starRating: temp["star_rating"],
+          contactEmail: temp["contact_email"],
+          phoneNumber: temp["phone_number"],
+          numberOfRooms: temp["number_of_rooms"],
+          manager: temp["manager"]
+        }
+        Hhotels.push(hotel);
+      }
+      setHotels(Hhotels);
+    });
+  }
+  function getHotelChains() {
+    fetch('http://localhost:3001/hotel_chain').then(response => {return response.text();})
+    .then(data => {
+      const obj = JSON.parse(data);
+      Hhotel_chains = [];
+      for (let i = 0; i < obj.length; i++) {
+        let temp = obj[i];
+        let chain = {
+          name: temp["name"],
+          numberOfHotels: temp["number_of_hotels"],
+          centralOfficeAddress: temp["central_office_address"]
+        }
+        Hhotel_chains.push(chain);
+      }
+      setHotelChain(Hhotel_chains);
+    })
+  }
+  function getBookings() {
+    fetch('http://localhost:3001/bookings').then(response => {
+      return response.text();
+    }).then(data => {
+      const obj = JSON.parse(data);
+      Bbookings = [];
+      for (let i = 0; i < obj.length; i++) {
+        let booking = {
+          bookingID: booking["booking_id"],
+          roomNumber: booking["room_number"],
+          startDate: booking["start_date"],
+          endDate: booking["end_date"],
+          customerID: booking["customer_id"]
+        }
+        Bbookings.push(booking);
+      }
+      setBookings(Bbookings)
+    })
+  }
   function Room({roomIT}) {
     return (
       <div class="listElem">
@@ -72,19 +245,27 @@ function SearchBar() {
       <form class="searchFields">
         <h3>Search</h3>
         <label for="startDate">Start Date</label>
-        <input type="text"/>
+        <div>
+        <input onChange={handleStartYChange} placeholder="YYYY" type="number"/>
+        <input onChange={handleStartMChange} placeholder="MM" type="number"/>
+        <input onChange={handleStartDChange} placeholder="DD" type="number"/>
+        </div>
         <label for="endDate">End Date</label>
-        <input type="text"/>
+        <div>
+        <input onChange={handleEndYChange} placeholder="YYYY" type="number"/>
+        <input onChange={handleEndMChange} placeholder="MM" type="number"/>
+        <input onChange={handleEndDChange} placeholder="DD" type="number"/>
+        </div>
         <label for="capacity">Capacity</label>
         <input onChange={handleCapacityChange} type="number"/>
         <label for="area">Area</label>
-        <input type="text"/>
+        <input onChange={handleAreaChange} type="text"/>
         <label for="hotelChain">Hotel Chain</label>
-        <input type="text"/>
+        <input onChange={handleChainChange} type="text"/>
         <label for="rating">Rating</label>
-        <input type="text"/>
+        <input onChange={handleRatingChange} type="number"/>
         <label for="numberOfRooms">Number Of Rooms</label>
-        <input type="text"/>
+        <input onChange={handleNumRoomsChange} type="number"/>
         <label for="PPN">Price of Rooms</label>
         <input onChange={handlePriceChange} type="text"/>
       </form>
