849dea666120c9e9749231cfcb6d531d02e41090
Rahul <114185111+Rahul-a13@users.noreply.github.com>
2023-04-09 21:13:40 -0400

    Update views.sql
    Added 'OR REPLACE' to make the view re-runnable

--- views.sql
+++ views.sql
@@ -5,14 +5,14 @@
 */
 
 -- View 1: Number of Available Rooms Per Area (Assume area refers to looking at same address {e.g. city})
-CREATE VIEW available_rooms_per_area AS
+CREATE OR REPLACE VIEW available_rooms_per_area AS
 SELECT h.address, COUNT(*) AS available_rooms
 FROM hotel h JOIN room r ON h.hotel_ID = r.hotel_ID
 GROUP BY address;
 
 
 -- View 2: Capacity of All Rooms of a Specific Hotel
-CREATE VIEW capacity_of_all_rooms AS
+CREATE OR REPLACE VIEW capacity_of_all_rooms AS
 SELECT h.name as hotel_name, r.room_number, r.capacity AS total_capacity_of_room
 FROM hotel h JOIN room r ON h.hotel_ID = r.hotel_ID
 WHERE h.hotel_ID='00001';
